1d613b77f57be3a9932498b065ee753a3345366e
triloksh07 <trilok0726@gmail.com>
2026-06-10 19:14:28 +0530

    fix(build): remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
 import GulfstreamLanding from './components/HomePage'
-// import Test from './components/Test.tsx'
-// import Test2 from './components/Test2.tsx'
 
 function App() {
 
   return (
     <>
       <GulfstreamLanding />
-      {/* <Test /> */}
-      {/* <Test2 /> */}
     </>
   )
 }
